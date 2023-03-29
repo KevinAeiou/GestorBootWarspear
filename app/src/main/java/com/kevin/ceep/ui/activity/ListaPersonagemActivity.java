ea10d8ed09f46660dece817c35040ce7b41ed47b
Kevin <kevin.souzaeiou@outlook.com>
2023-03-28 21:26:39 -0400

    Atualização do projeto.

--- app/src/main/java/com/kevin/ceep/ui/activity/ListaPersonagemActivity.java
+++ app/src/main/java/com/kevin/ceep/ui/activity/ListaPersonagemActivity.java
@@ -22,6 +22,8 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -33,6 +35,7 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.chip.Chip;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
@@ -66,7 +69,9 @@ public class ListaPersonagemActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private FirebaseDatabase database;
     private DatabaseReference minhaReferencia;
+    private Chip chip;
     private String usuarioId,nomePersonagem,emailPersonagem,senhaPersonagem;
+    private String[] mensagens={"Carregando dados...","Erro de conexão..."};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,11 +79,7 @@ public class ListaPersonagemActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lista_personagem);
         setTitle(CHAVE_TITULO_PERSONAGEM);
 
-        recebeDadosIntent();
-
         inicializaComponentes();
-
-        atualizaListaPersonagem();
 
         configuraCampoNovoPersonagem();
 
@@ -100,6 +101,7 @@ public class ListaPersonagemActivity extends AppCompatActivity {
         edtNovoPersonagem = findViewById(R.id.edtNovoPersonagem);
         edtEmailPersonagem = findViewById(R.id.edtEmailPersonagem);
         edtSenhaPersonagem = findViewById(R.id.edtSenhaPersonagem);
+        chip=findViewById(R.id.itemRadioButton);
     }
 
     private void configuraDeslizeItem() {
@@ -132,10 +134,18 @@ public class ListaPersonagemActivity extends AppCompatActivity {
     }
 
     private void atualizaListaPersonagem() {
-        configuraDialogoProgresso();
-        List<Personagem> todosPersonagens = pegaTodosPersonagens();
-        personagens.clear();
-        configuraRecyclerView(todosPersonagens);
+        configuraDialogoProgresso(0);
+        if (vericaConexaoInternet()) {
+            List<Personagem> todosPersonagens = pegaTodosPersonagens();
+            personagens.clear();
+            configuraRecyclerView(todosPersonagens);
+        }else{
+            progressDialog.dismiss();
+            //configuraDialogoProgresso(1);
+            //vaiParaEntraActivity();
+            //finish();
+            //progressDialog.dismiss();
+        }
     }
 
     @Override
@@ -197,6 +207,8 @@ public class ListaPersonagemActivity extends AppCompatActivity {
         super.onStart();
         FirebaseUser currentUser = minhaAutenticacao.getCurrentUser();
         minhaAutenticacao.updateCurrentUser(currentUser);
+        recebeDadosIntent();
+        atualizaListaPersonagem();
         Log.i(TAG_ACTIVITY,"onStartListaPersonagem");
     }
 
@@ -408,11 +420,20 @@ public class ListaPersonagemActivity extends AppCompatActivity {
         });
     }
 
-    private void configuraDialogoProgresso() {
+    private void configuraDialogoProgresso(int posicaoMensagem) {
         progressDialog = new ProgressDialog(this);
         progressDialog.setCancelable(false);
-        progressDialog.setMessage("Carregando dados...");
+        progressDialog.setMessage(mensagens[posicaoMensagem]);
         progressDialog.show();
+    }
+
+    private Boolean vericaConexaoInternet(){
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo infConexao = cm.getActiveNetworkInfo();
+        if(infConexao!=null && infConexao.isConnectedOrConnecting()){
+            return true;
+        }
+        return false;
     }
 
     private void configuraRecyclerView(List<Personagem> todosPersonagens) {
@@ -423,8 +444,9 @@ public class ListaPersonagemActivity extends AppCompatActivity {
 
     private void configuraAdapter(List<Personagem> todosPersonagens, RecyclerView recyclerView) {
         personagemAdapter = new ListaPersonagemAdapter(this,todosPersonagens);
+        progressDialog.dismiss();
         recyclerView.setAdapter(personagemAdapter);
-        personagemAdapter.setOnRadioButtonClickListener(new OnItemClickListener() {
+        personagemAdapter.setOnChipButtonClickListener(new OnItemClickListener() {
             @Override
             public void onItemClick(Profissao profissao, int posicao) {
 
@@ -432,8 +454,13 @@ public class ListaPersonagemActivity extends AppCompatActivity {
 
             @Override
             public void onItemClick(Personagem personagem, int posicao) {
-                List<Personagem> todosPersonagens = atualizaEstadoPersonagem(personagem);
-                configuraRecyclerView(todosPersonagens);
+                if (chip.isChecked()){
+                    List<Personagem> todosPersonagens = atualizaEstadoPersonagem(personagem,0);
+                    configuraRecyclerView(todosPersonagens);
+                }else {
+                    List<Personagem> todosPersonagens = atualizaEstadoPersonagem(personagem, 1);
+                    configuraRecyclerView(todosPersonagens);
+                }
             }
 
             @Override
@@ -441,7 +468,6 @@ public class ListaPersonagemActivity extends AppCompatActivity {
 
             }
         });
-
         personagemAdapter.setOnItemClickListener(new OnItemClickListener() {
             @Override
             public void onItemClick(Profissao profissao, int posicao) {
@@ -468,7 +494,7 @@ public class ListaPersonagemActivity extends AppCompatActivity {
         });
     }
 
-    private List<Personagem> atualizaEstadoPersonagem(Personagem raridade) {
+    private List<Personagem> atualizaEstadoPersonagem(Personagem raridade,int estado) {
         personagens = new ArrayList<>();
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference personagemReferencia = database.getReference(CHAVE_USUARIOS);
@@ -480,10 +506,7 @@ public class ListaPersonagemActivity extends AppCompatActivity {
                             Personagem personagem = dn.getValue(Personagem.class);
                             if (personagem.getId().equals(raridade.getId())){
                                 personagemReferencia.child(usuarioId).child(CHAVE_PERSONAGEM).
-                                        child(personagem.getId()).child("estado").setValue(1);
-                            }else{
-                                personagemReferencia.child(usuarioId).child(CHAVE_PERSONAGEM).
-                                        child(personagem.getId()).child("estado").setValue(0);
+                                        child(personagem.getId()).child("estado").setValue(estado);
                             }
                             personagens.add(personagem);
                         }
@@ -513,7 +536,6 @@ public class ListaPersonagemActivity extends AppCompatActivity {
                             personagens.add(personagem);
                         }
                         personagemAdapter.notifyDataSetChanged();
-                        progressDialog.dismiss();
                     }
 
                     @Override
