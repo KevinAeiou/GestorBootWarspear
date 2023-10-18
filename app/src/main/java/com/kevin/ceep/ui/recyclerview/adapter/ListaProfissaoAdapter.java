396e197d4a2c0ba05c3ebc84897a9d396060f41d
Kevin <kevin.souzaeiou@outlook.com>
2023-10-18 13:11:52 -0400

    Atualização defineExperienciaMaxima.

--- app/src/main/java/com/kevin/ceep/ui/recyclerview/adapter/ListaProfissaoAdapter.java
+++ app/src/main/java/com/kevin/ceep/ui/recyclerview/adapter/ListaProfissaoAdapter.java
@@ -1,15 +1,18 @@
 package com.kevin.ceep.ui.recyclerview.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.kevin.ceep.R;
+import com.kevin.ceep.model.Personagem;
 import com.kevin.ceep.model.Profissao;
 import com.kevin.ceep.ui.recyclerview.adapter.listener.OnItemClickListener;
 
@@ -53,12 +56,14 @@ public class ListaProfissaoAdapter extends RecyclerView.Adapter<ListaProfissaoAd
 
         private final TextView nome_profissao;
         private final TextView experiencia_profissao;
+        private final TextView nivel_profissao;
         private Profissao profissao;
 
         public ProfissaoViewHolder(@NonNull View itemView) {
             super(itemView);
             nome_profissao = itemView.findViewById(R.id.itemNomeProfissao);
             experiencia_profissao = itemView.findViewById(R.id.itemExperienciaProfissao);
+            nivel_profissao = itemView.findViewById(R.id.itemNivelProfissao);
 
             itemView.setOnClickListener(v -> onItemClickListener.onItemClick(profissao,getAdapterPosition()));
         }
@@ -69,68 +74,94 @@ public class ListaProfissaoAdapter extends RecyclerView.Adapter<ListaProfissaoAd
         }
 
         private void preencheCampo(Profissao profissao) {
-            String expMaximo = defineBarraExperiencia(profissao);
+            Integer expMaximo = defineExperienciaMaxima(profissao);
+            int restante=expMaximo-profissao.getExperiencia();
+            String exp= expMaximo+"-"+profissao.getExperiencia()+"="+restante;
             nome_profissao.setText(profissao.getNome());
-            experiencia_profissao.setText(expMaximo);
+            experiencia_profissao.setText(exp);
         }
 
         @NonNull
-        private String defineBarraExperiencia(Profissao profissao) {
-            String barraExperiencia="";
+        private Integer defineExperienciaMaxima(Profissao profissao) {
+            Integer expMaximo=0;
             if (profissao.getExperiencia()<20){
-                barraExperiencia=profissao.getExperiencia()+"/"+"20";
+                expMaximo=20;
+                nivel_profissao.setText("1");
             } else if (profissao.getExperiencia()<200) {
-                barraExperiencia=(profissao.getExperiencia()-20)+"/"+"179";
+                expMaximo=200;
+                nivel_profissao.setText("2");
             } else if (profissao.getExperiencia()<540) {
-                barraExperiencia=(profissao.getExperiencia()-200)+"/"+"339";
-            } else if (profissao.getExperiencia()<1250) {
-                barraExperiencia=(profissao.getExperiencia()-540)+"/"+"709";
+                expMaximo=1250;
+                nivel_profissao.setText("3");
             } else if (profissao.getExperiencia()<2550) {
-                barraExperiencia=(profissao.getExperiencia()-1250)+"/"+"1299";
+                expMaximo=2550;
+                nivel_profissao.setText("4");
             } else if (profissao.getExperiencia()<4700) {
-                barraExperiencia=(profissao.getExperiencia()-2550)+"/"+"2149";
+                expMaximo=4700;
+                nivel_profissao.setText("5");
             } else if (profissao.getExperiencia()<7990) {
-                barraExperiencia=(profissao.getExperiencia()-4700)+"/"+"3289";
+                expMaximo=7990;
+                nivel_profissao.setText("6");
             } else if (profissao.getExperiencia()<12770) {
-                barraExperiencia=(profissao.getExperiencia()-7990)+"/"+"4779";
+                expMaximo=12770;
+                nivel_profissao.setText("7");
             } else if (profissao.getExperiencia()<19440) {
-                barraExperiencia=(profissao.getExperiencia()-12770)+"/"+"6669";
+                expMaximo=19440;
+                nivel_profissao.setText("8");
             } else if (profissao.getExperiencia()<28440) {
-                barraExperiencia=(profissao.getExperiencia()-19440)+"/"+"8999";
+                expMaximo=28440;
+                nivel_profissao.setText("9");
             } else if (profissao.getExperiencia()<40270) {
-                barraExperiencia=(profissao.getExperiencia()-28440)+"/"+"11829";
+                expMaximo=40270;
+                nivel_profissao.setText("10");
             } else if (profissao.getExperiencia()<55450) {
-                barraExperiencia=(profissao.getExperiencia()-40270)+"/"+"15179";
+                expMaximo=55450;
+                nivel_profissao.setText("11");
             } else if (profissao.getExperiencia()<74570) {
-                barraExperiencia=(profissao.getExperiencia()-55450)+"/"+"19119";
+                expMaximo=74570;
+                nivel_profissao.setText("12");
             } else if (profissao.getExperiencia()<98250) {
-                barraExperiencia=(profissao.getExperiencia()-74570)+"/"+"23679";
+                expMaximo=98250;
+                nivel_profissao.setText("13");
             } else if (profissao.getExperiencia()<127180) {
-                barraExperiencia=(profissao.getExperiencia()-98250)+"/"+"28929";
+                expMaximo=127180;
+                nivel_profissao.setText("14");
             } else if (profissao.getExperiencia()<156110) {
-                barraExperiencia=(profissao.getExperiencia()-127180)+"/"+"28929";
+                expMaximo=156110;
+                nivel_profissao.setText("15");
             } else if (profissao.getExperiencia()<185040) {
-                barraExperiencia=(profissao.getExperiencia()-156110)+"/"+"28929";
+                expMaximo=185040;
+                nivel_profissao.setText("16");
             } else if (profissao.getExperiencia()<215001) {
-                barraExperiencia=(profissao.getExperiencia()-185040)+"/"+"29959";
+                expMaximo=215001;
+                nivel_profissao.setText("17");
             } else if (profissao.getExperiencia()<245000) {
-                barraExperiencia=(profissao.getExperiencia()-215001)+"/"+"29999";
+                expMaximo=245000;
+                nivel_profissao.setText("18");
             } else if (profissao.getExperiencia()<300000) {
-                barraExperiencia=(profissao.getExperiencia()-245000)+"/"+"54999";
+                expMaximo=300000;
+                nivel_profissao.setText("19");
             } else if (profissao.getExperiencia()<375000) {
-                barraExperiencia=(profissao.getExperiencia()-300000)+"/"+"74999";
+                expMaximo=375000;
+                nivel_profissao.setText("20");
             } else if (profissao.getExperiencia()<470000) {
-                barraExperiencia=(profissao.getExperiencia()-375000)+"/"+"94999";
+                expMaximo=470000;
+                nivel_profissao.setText("21");
             } else if (profissao.getExperiencia()<585000) {
-                barraExperiencia=(profissao.getExperiencia()-470000)+"/"+"114999";
+                expMaximo=585000;
+                nivel_profissao.setText("22");
             } else if (profissao.getExperiencia()<720000) {
-                barraExperiencia=(profissao.getExperiencia()-585000)+"/"+"134999";
+                expMaximo=720000;
+                nivel_profissao.setText("23");
             } else if (profissao.getExperiencia()<875000) {
-                barraExperiencia=(profissao.getExperiencia()-720000)+"/"+"154999";
+                expMaximo=875000;
+                nivel_profissao.setText("24");
             } else if (profissao.getExperiencia()<105000) {
-                barraExperiencia=(profissao.getExperiencia()-875000)+"/"+"174999";
-            }
-            return barraExperiencia;
+                expMaximo = 105000;
+                nivel_profissao.setText("25");
+            }else
+                nivel_profissao.setText("26");
+            return expMaximo;
         }
 
     }
