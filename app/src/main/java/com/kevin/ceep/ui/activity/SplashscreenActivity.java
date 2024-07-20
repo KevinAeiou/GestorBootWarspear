d6948ef3daa0b9468aec41fdf7364f7196292fa0
KevinAeiou <kevin.souzaeiou@outlook.com>
2024-07-20 11:28:42 -0400

    Implementar a função que verifica se deve mostrar ou não a Splashscreen ao usuário.

--- app/src/main/java/com/kevin/ceep/ui/activity/SplashscreenActivity.java
+++ app/src/main/java/com/kevin/ceep/ui/activity/SplashscreenActivity.java
@@ -1,6 +1,8 @@
 package com.kevin.ceep.ui.activity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -9,13 +11,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.kevin.ceep.R;
 
+@SuppressLint("CustomSplashScreen")
 public class SplashscreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+        SharedPreferences preferences = getSharedPreferences("user_preferences", MODE_PRIVATE);
+
+        if (preferences.contains("ja_abriu_app")) {
+            vaiParaEntraUsuarioActivity();
+        } else {
+            adicionarPreferenceJaAbriu(preferences);
+            mostrarSplash();
+        }
+    }
+
+    private void mostrarSplash() {
         Handler handler = new Handler();
-        handler.postDelayed(() -> vaiParaEntraUsuarioActivity(), 3000);
+        handler.postDelayed(this::vaiParaEntraUsuarioActivity, 3000);
+    }
+
+    private void adicionarPreferenceJaAbriu(SharedPreferences preferences) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("ja_abriu_app", true);
+        editor.apply();
     }
 
     private void vaiParaEntraUsuarioActivity() {
